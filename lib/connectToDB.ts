3fea0a18b0e81bfb8c568d11276b859902501660
Sylvanus-the-sorcerer <103826382+sepehrsylvanus@users.noreply.github.com>
2025-06-06 00:16:02 +0300

    connection to DB

---
+++ lib/connectToDB.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+  if (isConnected) {
+    console.log("Using existing MongoDB connection");
+    return;
+  }
+  if (mongoose.connections.length > 0) {
+    const connectionState = mongoose.connections[0].readyState;
+    if (connectionState === 1) {
+      console.log("MongoDB already connected");
+      isConnected = true;
+      return;
+    }
+    await mongoose.disconnect();
+  }
+
+  try {
+    const connection = await mongoose.connect(
+      process.env.NEXT_PUBLIC_MONGODB_URI!
+    );
+    isConnected = true;
+    if (connection) {
+      console.log("Successfully connected to database");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
