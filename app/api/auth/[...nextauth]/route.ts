3fc61416dae4b5bafc6a7a5f302d7f184f434390
Sylvanus-the-sorcerer <103826382+sepehrsylvanus@users.noreply.github.com>
2025-06-07 20:23:32 +0300

    fix google auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,7 +4,7 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import Email from "next-auth/providers/email";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
